ca1cdbd9fddced1a092187134330b238fc961b96
Andrey Baksheev <andrey_baksheev@mail.ru>
2019-03-03 00:21:17 +0400

    VacationApp test project

---
+++ VacationsApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using VacationsLib.Model;
+
+
+namespace VacationsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (VacationsContext context = new VacationsContext())
+                { 
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
